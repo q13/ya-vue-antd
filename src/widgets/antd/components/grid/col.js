8c3facb76bf05116326d6d28e4d5382f85e745cb
q13 <qsimon13@gmail.com>
2018-12-19 15:35:25 +0800

    Grid api edit

--- src/widgets/antd/components/grid/col.js
+++ src/widgets/antd/components/grid/col.js
@@ -14,5 +14,15 @@ import Col from 'antd/lib/col';
 
 /**
  * @vue-prop {number} [offset=0] - 栅格左侧的间隔格数，间隔内不可以有栅格
+ * @vue-prop {number} [order=0] - 栅格顺序，flex 布局模式下有效
+ * @vue-prop {number} [pull=0] - 栅格向左移动格数
+ * @vue-prop {number} [push=0] - 栅格向右移动格数
+ * @vue-prop {number} [span] - 栅格占位格数，为 0 时相当于 display: none
+ * @vue-prop {number|object} [xs] - <576px 响应式栅格，可为栅格数或一个包含其他属性的对象
+ * @vue-prop {number|object} [sm] - ≥576px 响应式栅格，可为栅格数或一个包含其他属性的对象
+ * @vue-prop {number|object} [md] - ≥768px 响应式栅格，可为栅格数或一个包含其他属性的对象
+ * @vue-prop {number|object} [lg] - ≥992px 响应式栅格，可为栅格数或一个包含其他属性的对象
+ * @vue-prop {number|object} [xl] - ≥1200px 响应式栅格，可为栅格数或一个包含其他属性的对象
+ * @vue-prop {number|object} [xxl] - ≥1600px 响应式栅格，可为栅格数或一个包含其他属性的对象
  */
 export default t(Col);
--- src/widgets/antd/components/grid/row.js
+++ src/widgets/antd/components/grid/row.js
@@ -14,5 +14,8 @@ import Row from 'antd/lib/row';
 
 /**
  * @vue-prop {string} [align=top] - flex 布局下的垂直对齐方式：top middle bottom
+ * @vue-prop {number|object} [gutter=0] - 栅格间隔，可以写成像素值或支持响应式的对象写法 { xs: 8, sm: 16, md: 24}
+ * @vue-prop {string} [justify=start] - flex 布局下的水平排列方式：start end center space-around space-between
+ * @vue-prop {string} [type] - 布局模式，可选 flex，[现代浏览器]{@link https://caniuse.com/#search=flex} 下有效
  */
 export default t(Row);
